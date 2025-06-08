adc0aa6aa7295d1dbcae942cc6e6dba2618a7425
Michael Nahkies <michael@nahkies.co.nz>
2025-06-08 20:02:32 +0100

    feat: better filter parsing (#7)
    implements a more comprehensive filter parser using peggy.
    
    most of the changes in this PR (well the test cases + grammar changes)
    were actually implemented by ChatGPT
    (https://chatgpt.com/share/6845d711-5adc-8007-acef-910c8e8618dd) - this
    is notable, as when I first implemented this I tried to get ChatGPT to
    help me and it failed spectacularly.
    
    Based on VCS that was roughly September last year, or about 9 months ago
    - apparently it's gotten much better at this kind of problem in that
    time.

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,5 +1,5 @@
 import {describe, expect, it} from "@jest/globals"
-import {getType} from "./utils"
+import {evaluateFilter, getType, parseFilter} from "./utils"
 
 describe("utils", () => {
   describe("#getType", () => {
@@ -13,6 +13,502 @@ describe("utils", () => {
 
     it("returns 'single-value' when obj is primitive and value is primitive", () => {
       expect(getType("abc", 123)).toBe("single-value")
+    })
+  })
+
+  describe("#parseFilter", () => {
+    it("parses basic equality", () => {
+      const ast = parseFilter('userName eq "bjensen"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "userName",
+          type: "attrPath",
+        },
+        operator: "eq",
+        type: "comparison",
+        value: "bjensen",
+      })
+    })
+
+    it("parses co substring match", () => {
+      const ast = parseFilter('name.familyName co "O\'Malley"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "name.familyName",
+          type: "attrPath",
+        },
+        operator: "co",
+        type: "comparison",
+        value: "O'Malley",
+      })
+    })
+
+    it("parses sw (starts with)", () => {
+      const ast = parseFilter('userName sw "J"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "userName",
+          type: "attrPath",
+        },
+        operator: "sw",
+        type: "comparison",
+        value: "J",
+      })
+    })
+
+    // todo: support schema qualification correctly
+    it.skip("parses schema-qualified attrPath", () => {
+      const ast = parseFilter(
+        'urn:ietf:params:scim:schemas:core:2.0:User:userName sw "J"',
+      )
+      expect(ast).toMatchInlineSnapshot()
+    })
+
+    it("parses presence", () => {
+      const ast = parseFilter("title pr")
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "title",
+          type: "attrPath",
+        },
+        type: "present",
+      })
+    })
+
+    it("parses greater than date", () => {
+      const ast = parseFilter('meta.lastModified gt "2011-05-13T04:42:34Z"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "meta.lastModified",
+          type: "attrPath",
+        },
+        operator: "gt",
+        type: "comparison",
+        value: "2011-05-13T04:42:34Z",
+      })
+    })
+
+    it("parses greater or equal", () => {
+      const ast = parseFilter('meta.lastModified ge "2011-05-13T04:42:34Z"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "meta.lastModified",
+          type: "attrPath",
+        },
+        operator: "ge",
+        type: "comparison",
+        value: "2011-05-13T04:42:34Z",
+      })
+    })
+
+    it("parses less than", () => {
+      const ast = parseFilter('meta.lastModified lt "2011-05-13T04:42:34Z"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "meta.lastModified",
+          type: "attrPath",
+        },
+        operator: "lt",
+        type: "comparison",
+        value: "2011-05-13T04:42:34Z",
+      })
+    })
+
+    it("parses less or equal", () => {
+      const ast = parseFilter('meta.lastModified le "2011-05-13T04:42:34Z"')
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "meta.lastModified",
+          type: "attrPath",
+        },
+        operator: "le",
+        type: "comparison",
+        value: "2011-05-13T04:42:34Z",
+      })
+    })
+
+    it("parses and expression", () => {
+      const ast = parseFilter('title pr and userType eq "Employee"')
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "title",
+            type: "attrPath",
+          },
+          type: "present",
+        },
+        operator: "and",
+        right: {
+          attribute: {
+            path: "userType",
+            type: "attrPath",
+          },
+          operator: "eq",
+          type: "comparison",
+          value: "Employee",
+        },
+        type: "logical",
+      })
+    })
+
+    it("parses or expression", () => {
+      const ast = parseFilter('title pr or userType eq "Intern"')
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "title",
+            type: "attrPath",
+          },
+          type: "present",
+        },
+        operator: "or",
+        right: {
+          attribute: {
+            path: "userType",
+            type: "attrPath",
+          },
+          operator: "eq",
+          type: "comparison",
+          value: "Intern",
+        },
+        type: "logical",
+      })
+    })
+
+    it("parses long schema filter", () => {
+      const ast = parseFilter(
+        'schemas eq "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"',
+      )
+      expect(ast).toStrictEqual({
+        attribute: {
+          path: "schemas",
+          type: "attrPath",
+        },
+        operator: "eq",
+        type: "comparison",
+        value: "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User",
+      })
+    })
+
+    it("parses and with grouped or", () => {
+      const ast = parseFilter(
+        'userType eq "Employee" and (emails co "example.com" or emails.value co "example.org")',
+      )
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "userType",
+            type: "attrPath",
+          },
+          operator: "eq",
+          type: "comparison",
+          value: "Employee",
+        },
+        operator: "and",
+        right: {
+          left: {
+            attribute: {
+              path: "emails",
+              type: "attrPath",
+            },
+            operator: "co",
+            type: "comparison",
+            value: "example.com",
+          },
+          operator: "or",
+          right: {
+            attribute: {
+              path: "emails.value",
+              type: "attrPath",
+            },
+            operator: "co",
+            type: "comparison",
+            value: "example.org",
+          },
+          type: "logical",
+        },
+        type: "logical",
+      })
+    })
+
+    it("parses and with not and grouped or", () => {
+      const ast = parseFilter(
+        'userType ne "Employee" and not (emails co "example.com" or emails.value co "example.org")',
+      )
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "userType",
+            type: "attrPath",
+          },
+          operator: "ne",
+          type: "comparison",
+          value: "Employee",
+        },
+        operator: "and",
+        right: {
+          expression: {
+            left: {
+              attribute: {
+                path: "emails",
+                type: "attrPath",
+              },
+              operator: "co",
+              type: "comparison",
+              value: "example.com",
+            },
+            operator: "or",
+            right: {
+              attribute: {
+                path: "emails.value",
+                type: "attrPath",
+              },
+              operator: "co",
+              type: "comparison",
+              value: "example.org",
+            },
+            type: "logical",
+          },
+          type: "not",
+        },
+        type: "logical",
+      })
+    })
+
+    it("parses and with grouped sub-attr", () => {
+      const ast = parseFilter(
+        'userType eq "Employee" and (emails.type eq "work")',
+      )
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "userType",
+            type: "attrPath",
+          },
+          operator: "eq",
+          type: "comparison",
+          value: "Employee",
+        },
+        operator: "and",
+        right: {
+          attribute: {
+            path: "emails.type",
+            type: "attrPath",
+          },
+          operator: "eq",
+          type: "comparison",
+          value: "work",
+        },
+        type: "logical",
+      })
+    })
+
+    it("parses valuePath with and inside", () => {
+      const ast = parseFilter(
+        'userType eq "Employee" and emails[type eq "work" and value co "@example.com"]',
+      )
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "userType",
+            type: "attrPath",
+          },
+          operator: "eq",
+          type: "comparison",
+          value: "Employee",
+        },
+        operator: "and",
+        right: {
+          attribute: {
+            path: "emails",
+            type: "attrPath",
+          },
+          filter: {
+            left: {
+              attribute: {
+                path: "type",
+                type: "attrPath",
+              },
+              operator: "eq",
+              type: "comparison",
+              value: "work",
+            },
+            operator: "and",
+            right: {
+              attribute: {
+                path: "value",
+                type: "attrPath",
+              },
+              operator: "co",
+              type: "comparison",
+              value: "@example.com",
+            },
+            type: "logical",
+          },
+          type: "valuePath",
+        },
+        type: "logical",
+      })
+    })
+
+    it("parses or between valuePaths", () => {
+      const ast = parseFilter(
+        'emails[type eq "work" and value co "@example.com"] or ims[type eq "xmpp" and value co "@foo.com"]',
+      )
+      expect(ast).toStrictEqual({
+        left: {
+          attribute: {
+            path: "emails",
+            type: "attrPath",
+          },
+          filter: {
+            left: {
+              attribute: {
+                path: "type",
+                type: "attrPath",
+              },
+              operator: "eq",
+              type: "comparison",
+              value: "work",
+            },
+            operator: "and",
+            right: {
+              attribute: {
+                path: "value",
+                type: "attrPath",
+              },
+              operator: "co",
+              type: "comparison",
+              value: "@example.com",
+            },
+            type: "logical",
+          },
+          type: "valuePath",
+        },
+        operator: "or",
+        right: {
+          attribute: {
+            path: "ims",
+            type: "attrPath",
+          },
+          filter: {
+            left: {
+              attribute: {
+                path: "type",
+                type: "attrPath",
+              },
+              operator: "eq",
+              type: "comparison",
+              value: "xmpp",
+            },
+            operator: "and",
+            right: {
+              attribute: {
+                path: "value",
+                type: "attrPath",
+              },
+              operator: "co",
+              type: "comparison",
+              value: "@foo.com",
+            },
+            type: "logical",
+          },
+          type: "valuePath",
+        },
+        type: "logical",
+      })
+    })
+  })
+
+  describe("#evaluateFilter", () => {
+    const baseUser = {
+      userName: "bjensen",
+      name: {familyName: "O'Malley"},
+      title: "Manager",
+      meta: {lastModified: "2011-05-14T04:42:34Z"},
+      userType: "Employee",
+      emails: [
+        {type: "work", value: "bjensen@example.com"},
+        {type: "home", value: "bjensen@example.org"},
+      ],
+    }
+
+    it("matches basic equality", () => {
+      const ast = parseFilter('userName eq "bjensen"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it.skip("fails case-sensitive equality", () => {
+      const ast = parseFilter('userName eq "BJENSEN"')
+      expect(evaluateFilter(ast, baseUser)).toBe(false)
+    })
+
+    it("matches case-insensitive substring match", () => {
+      const ast = parseFilter('name.familyName co "malley"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches mixed-case substring", () => {
+      const ast = parseFilter('name.familyName co "Malley"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches upper-case substring due to caseExact: false", () => {
+      const ast = parseFilter('name.familyName co "MALLEY"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches presence check", () => {
+      const ast = parseFilter("title pr")
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches gt date", () => {
+      const ast = parseFilter('meta.lastModified gt "2011-05-13T00:00:00Z"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches lt date", () => {
+      const ast = parseFilter('meta.lastModified lt "2011-05-15T00:00:00Z"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches logical and", () => {
+      const ast = parseFilter('title pr and userType eq "Employee"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches logical or", () => {
+      const ast = parseFilter('title pr or userType eq "Intern"')
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches valuePath filter", () => {
+      const ast = parseFilter(
+        'emails[type eq "work" and value co "@example.com"]',
+      )
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it("matches valuePath with uppercase domain (case-insensitive)", () => {
+      const ast = parseFilter(
+        'emails[type eq "work" and value co "@EXAMPLE.COM"]',
+      )
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
+    })
+
+    it.skip("fails if value is case-sensitive", () => {
+      const ast = parseFilter(
+        'emails[type eq "work" and value co "@EXAMPLE.COM"]',
+      )
+      expect(evaluateFilter(ast, baseUser)).toBe(false)
+    })
+
+    it("matches not with nested or", () => {
+      const ast = parseFilter(
+        'userType ne "Intern" and not (emails.value co "example.net")',
+      )
+      expect(evaluateFilter(ast, baseUser)).toBe(true)
     })
   })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -2,12 +2,158 @@ import {parse} from "../parser.js"
 import {PatchError} from "./errors"
 import type {t_PatchOperation} from "./generated/models"
 
-export function parseFilter(filter: string): {
-  left: string
-  right: string
-  operator: "eq" | "ne" | "co" | "sw" | "ew" | "gt" | "lt" | "ge" | "le"
-} {
+export type FilterOperator =
+  | "eq"
+  | "ne"
+  | "co"
+  | "sw"
+  | "ew"
+  | "gt"
+  | "lt"
+  | "ge"
+  | "le"
+
+export type FilterLogicalOperator = "and" | "or"
+
+export type FilterAttribute = {
+  path: string
+  type: "attrPath"
+}
+
+export type FilterPresent = {
+  attribute: FilterAttribute
+  type: "present"
+}
+
+export type FilterLogical = {
+  type: "logical"
+  operator: FilterLogicalOperator
+  left: FilterAST
+  right: FilterAST
+}
+
+export type FilterComparison = {
+  type: "comparison"
+  operator: FilterOperator
+  value: string
+  attribute: FilterAttribute
+}
+
+export type FilterNot = {
+  type: "not"
+  expression: FilterAST
+}
+
+export type FilterValuePath = {
+  type: "valuePath"
+  attribute: FilterAttribute
+  filter: FilterAST
+}
+
+export type FilterAST =
+  | FilterPresent
+  | FilterComparison
+  | FilterLogical
+  | FilterNot
+  | FilterValuePath
+
+export function parseFilter(filter: string): FilterAST {
   return parse(filter, undefined)
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function resolveAttrPath(obj: any, path: string): any {
+  return path.split(".").reduce((acc, key) => acc?.[key], obj)
+}
+
+function matchComparison(
+  operator: FilterOperator,
+  left: string | number,
+  right: string | number,
+  // TODO: apply caseInsensitive based on schema "caseExact"
+  caseInsensitive = true,
+): boolean {
+  if (
+    caseInsensitive &&
+    typeof left === "string" &&
+    typeof right === "string"
+  ) {
+    // biome-ignore lint/style/noParameterAssign: easier
+    left = left.toLowerCase()
+    // biome-ignore lint/style/noParameterAssign: easier
+    right = right.toLowerCase()
+  }
+
+  switch (operator) {
+    case "eq":
+      return left === right
+    case "ne":
+      return left !== right
+    case "co":
+      return (
+        typeof left === "string" &&
+        typeof right === "string" &&
+        left.includes(right)
+      )
+    case "sw":
+      return (
+        typeof left === "string" &&
+        typeof right === "string" &&
+        left.startsWith(right)
+      )
+    case "ew":
+      return (
+        typeof left === "string" &&
+        typeof right === "string" &&
+        left.endsWith(right)
+      )
+    case "gt":
+      return left > right
+    case "lt":
+      return left < right
+    case "ge":
+      return left >= right
+    case "le":
+      return left <= right
+    default:
+      throw new Error(`Unknown operator "${operator}"`)
+  }
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function evaluateFilter(ast: FilterAST, obj: any): boolean {
+  switch (ast.type) {
+    case "present": {
+      const val = resolveAttrPath(obj, ast.attribute.path)
+      return val !== undefined && val !== null
+    }
+    case "comparison": {
+      const val = resolveAttrPath(obj, ast.attribute.path)
+      return matchComparison(ast.operator, val, ast.value)
+    }
+    case "logical": {
+      const left = evaluateFilter(ast.left, obj)
+      const right = evaluateFilter(ast.right, obj)
+      return ast.operator === "and" ? left && right : left || right
+    }
+    case "not": {
+      return !evaluateFilter(ast.expression, obj)
+    }
+    case "valuePath": {
+      const collection = resolveAttrPath(obj, ast.attribute.path)
+
+      if (!Array.isArray(collection)) {
+        return false
+      }
+
+      return collection.some((item) => evaluateFilter(ast.filter, item))
+    }
+
+    default: {
+      // @ts-expect-error: exhaustive check
+      throw new Error(`unsupported type '${ast.type}' `)
+    }
+  }
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -46,7 +192,7 @@ export function performPatchOperation(
 
   if (!lastPart && op === "remove") {
     throw new PatchError(
-      "must specify a path on 'remove' operatoins",
+      "must specify a path on 'remove' operations",
       "noTarget",
     )
   }
